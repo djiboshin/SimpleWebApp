f019e518b0992ba49ae07780b76a8156c58e4881
djiboshin <64642744+djiboshin@users.noreply.github.com>
2023-08-02 15:58:39 +0300

    backend to frontend

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import time
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
@@ -10,4 +23,5 @@ def read_root():
 
 @app.get("/f")
 def read_item():
+    time.sleep(5)
     return [[1, 1, 1, 1], [1, 2, 3, 4], [4, 4, 4, 4]]
